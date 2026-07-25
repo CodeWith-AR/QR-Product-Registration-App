9451368928cc4281f3627212d191e4b17fbe7ed2
Abdur Rehman <mailrehman90527300@gmail.com>
2026-07-26 01:23:02 +0500

    Remove FirebaseOptions for web and android
    Removed FirebaseOptions for web and android platforms.

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -24,20 +24,4 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyBlTXN4IyTE3NnMG8OoicR4l04P7iVCuew',
-    appId: '1:223196297451:web:3ca9110d7dbb805031a6ae',
-    messagingSenderId: '223196297451',
-    projectId: 'authenticheck-v1-rb3554640',
-    authDomain: 'authenticheck-v1-rb3554640.firebaseapp.com',
-    storageBucket: 'authenticheck-v1-rb3554640.firebasestorage.app',
-  );
-
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyCjqqns-f4fvDp-TaOkgF6-_N2ko90Qr34',
-    appId: '1:223196297451:android:14ede2fa4d9bd0e431a6ae',
-    messagingSenderId: '223196297451',
-    projectId: 'authenticheck-v1-rb3554640',
-    storageBucket: 'authenticheck-v1-rb3554640.firebasestorage.app',
-  );
-}
+ Add Yours
